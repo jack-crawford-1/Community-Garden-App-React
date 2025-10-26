4a4a49633e820ad27cb354b52b3964ed722941ba
Jack <primes_unit.05@icloud.com>
2025-10-26 14:44:01 +1300

    added link to map page

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -10,20 +10,33 @@ function App() {
             <p className="uppercase">Grow Local</p>
           </div>
 
-          <div className="flex flex-col justify-center items-start text-lg ml-40">
-            <p className="pb-3 pangolin-regular">
-              Frontend: React, TypeScript, Tailwind CSS, Vite, Vis.gl, Google
-              Maps API, Supercluster, jwt-decode
+          <div className="flex flex-col justify-center items-start text-lg ml-60">
+            <p className="pb-3 ">
+              <span className="font-semibold">Frontend</span>: React,
+              TypeScript, Tailwind CSS, Vite, Vis.gl, Google Maps API,
+              Supercluster, jwt-decode
             </p>
-            <p className=" text-center  pb-3 pangolin-regular">
-              Backend: Express, MongoDB, Mongoose
+            <p className=" text-center  pb-3 ">
+              <span className="font-semibold">Backend</span>: Express, MongoDB,
+              Mongoose
             </p>
-            <p className=" text-center  pb-3 pangolin-regular">
-              Auth & Security: JWT (jsonwebtoken), bcrypt, CORS
+            <p className=" text-center  pb-3 ">
+              <span className="font-semibold">Auth & Security</span>: JWT
+              (jsonwebtoken), bcrypt, CORS
             </p>
-            <p className=" text-center  pb-3 pangolin-regular">
-              Infrastructure & Tools: Google Cloud Storage, Multer, Dotenv
+            <p className=" text-center  pb-3 ">
+              <span className="font-semibold">Infrastructure & Tools</span>:
+              Google Cloud Storage, Multer, Dotenv
             </p>
+            <p className=" text-center  pb-3 ">
+              <span className="font-semibold">Deployed</span>: Vercel Client,
+              Railway API
+            </p>
+            <a href="/map">
+              <button className="mt-10 bg-white hover:bg-[#55b47e] hover:text-white text-green-600 font-bold py-2 px-4 border-b-4 border-green-800 hover:border-white rounded">
+                See Gardens
+              </button>
+            </a>
           </div>
         </div>
       </div>
